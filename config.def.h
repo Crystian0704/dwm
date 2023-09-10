3f9f0e885f019d128c10a5b6af2816e86b80730a
Crystian Pereira <79256218+Crystian0704@users.noreply.github.com>
2023-09-09 23:13:00 -0300

    altera atalho

--- config.def.h
+++ config.def.h
@@ -83,8 +83,8 @@ static const Key keys[] = {
         /* modifier                     key        function        argument */
         { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
         { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+        { MODKEY,                       XK_x,      spawn,          {.v = xmenu-start } },
         { MODKEY,                       XK_o,      spawn,            SHCMD("slock") },
-        { MODKEY,                       XK_x,      spawn,          {.v = xmenu-start }},
         { MODKEY,                       XK_b,      togglebar,      {0} },
         { MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
         { MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
