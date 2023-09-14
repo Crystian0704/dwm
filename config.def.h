09b0543f156ce98cdbfe0103a59fce17d363503e
Crystian Pereira <79256218+Crystian0704@users.noreply.github.com>
2023-09-14 11:31:38 -0300

    modifica gaps

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 25;       /* snap pixel */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 5;       /* snap pixel */
 static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *panel_str        = "tint2";
@@ -31,7 +31,6 @@ static const Rule rules[] = {
          *      WM_NAME(STRING) = title
          */
         /* class      instance    title       tags mask     isfloating   monitor */
-        { "Gimp",     NULL,       NULL,       0,            1,           -1 },
         { "Tint2",    NULL,       NULL,       511,          0,           -1 }, // Show tint2 on every page
 };
 
@@ -47,8 +46,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
         /* symbol     arrange function */
-        { "[]=",      tile },    /* first entry is default */
         { "[M]",      monocle },
+        { "[]=",      tile },    /* first entry is default */
         { "[@]",      spiral },
         { "[\\]",     dwindle },
         { "H[]",      deck },
