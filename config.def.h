2a02fde8b28fdbab4fcd101944a606174a9626b8
Crystian Pereira <79256218+Crystian0704@users.noreply.github.com>
2023-10-07 09:03:48 -0300

    a

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static       int smartgaps          = 1;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 char *fonts[]                       = { "ProFont IIx Nerd Font Propo:size=10:autohint=true:antialias=true", "Noto Color Emoji:pixelsize=10:autohint=true:antialias=true" };
-static const char dmenufont[]      = { "ProFont IIx Nerd Font Propo:size=10:autohint=true:antialias=true", "Noto Color Emoji:pixelsize=10:autohint=true:antialias=true" };
+static const char dmenufont[]      = { "ProFont IIx Nerd Font Propo:size=10:autohint=true:antialias=true" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
